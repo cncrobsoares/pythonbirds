4decb91ebdef92b874f112d2b2610bb1f461c129
ben <mu_niaco@outlook.com>
2019-09-27 16:36:27 -0300

    ccp2

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,2 +1,37 @@
 class Pessoa:
+    olhos = 2
+
+    def __init__(self, *filhos, nome=None, idade=57):
+        self.idade = idade
+        self.nome = nome
+        self.filhos = list(filhos)
+
+    def cumprimentar(self):
+        return f"Olá!!! {id(self)} {self.nome} {self.idade}"
+
+    def __str__(self):
+        return self.nome
+
+
+class Homen(Pessoa):
+    # def __init__(self):
+    #     self.idade=35
     pass
+
+
+class Mutante(Pessoa):
+    olhos = 3
+
+
+if __name__ == "__main__":
+
+    dav = Homen(nome='David')
+    ben = Mutante(dav, nome='ben')
+    print(Pessoa.cumprimentar(ben))
+    for filho in ben.filhos:
+        print(filho)
+    print(ben.__dict__)
+    print(dav.__dict__)
+
+    print(ben.cumprimentar())
+    print(ben.olhos)
